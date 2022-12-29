b7af1121409d4537a6b6f49d9d4f44b3d21f883c
jmoro0408 <james.moro.11@gmail.com>
2022-12-29 18:13:13 +0000

    added filler words to stopwords

--- 2_topic_modelling.py
+++ 2_topic_modelling.py
@@ -9,9 +9,6 @@ from spacy.lang.en.stop_words import STOP_WORDS
 
 from utils import append_to_txt_file, list_from_text, read_from_db, read_toml
 
-# TODO Update custom stopwords to non-duplicates
-
-
 def most_frequent_words(docs: list[str], percent: int) -> list[str]:
     """Returns the most frequent x percentage of words in a corpus.
     i.e a percent value of 10 will return the most frequent 10% of words.
@@ -77,23 +74,28 @@ def run_LDA(docs: list[str], **kwargs) -> gensim.models.ldamodel.LdaModel:
     return Lda(doc_term_matrix, id2word=index_dictionary, **kwargs)
 
 
-def read_transcripts(config_dict: dict, row_limit: int = 50 ) -> list[str]:
+def read_transcripts(config_dict: dict, row_limit:Optional[int] = None) -> list[str]:
     """Reads in the podcast transcripts from postgresql db to a list.
 
     Args:
         config_dict (dict): Config dict with DB parameters.
-        row_limit (int): Number of rows to receive. Defaults to 50
+        row_limit (Optionsal(int)): Number of rows to receive. If None, all
+        rows are returned. Defaults to None.
 
     Returns:
         list[str]: List of transcripts.
     """
-    _query = """SELECT * from episodes LIMIT {}""".format(row_limit)
+    _query = """SELECT * from episodes"""
+    if row_limit is not None:
+        _query = """SELECT * from episodes LIMIT {}""".format(row_limit)
     df = read_from_db(_query, config_dict=config_dict)
     return df["transcript"].to_list()
 
 
 def prepare_stopwords(
-    full_corpus: list[str], percent_frequent_words: Union[int, float] = 1.5
+    full_corpus: list[str],
+    percent_frequent_words: Union[int, float] = 1.5,
+    **kwargs
 ) -> None:
     """Prepares custom stopwords from the full corpus.
     Uses the full corpus to findt he top x% of words (default at 1.5%) at use these as stopwords.
@@ -101,28 +103,33 @@ def prepare_stopwords(
     Args:
         full_corpus (list[str]): Full list of texts to use to detect stopwords.
         percent_frequent_words (Union[int,float]): % of most frequent words to look for. Defaults to 1.5%
-
+        **kwargs: keyword arguments available to inner append_to_txt_file function.
     Returns:
         List[str]: List of most frequent words.
     """
     if isinstance(full_corpus, str):
         full_corpus = full_corpus.split()
-    append_to_stopwords = ",".join(
-        most_frequent_words(full_corpus, percent_frequent_words)
-    )
+    most_frequent_words_list = most_frequent_words(full_corpus, percent_frequent_words)
     # Appending most frequent 1.5% of words to custom stopwords. 1.5% value has been found through trial and error.
-    append_to_txt_file(append_to_stopwords, r"custom_stopwords.txt")
+    filler_words = ['like', 'yeah', 'um', 'eh', 'actually', 'see', 'well', 'er',
+                    'said', 'right'] #spoken word fillers
+    append_to_stopwords = ",".join(most_frequent_words_list + filler_words)
+    append_to_txt_file(append_to_stopwords, r"custom_stopwords.txt",**kwargs)
     return list_from_text(r"custom_stopwords.txt")
 
 
 def main():
     NUM_TOPICS = 5
-    NUM_ROWS = 50
+    NUM_ROWS = 1
     config_dict = read_toml(r"db_info.toml")["database"] #config dict to access db
     texts = read_transcripts(config_dict, row_limit=NUM_ROWS)
-    custom_stopwords = prepare_stopwords(full_corpus=texts)
+    custom_stopwords = prepare_stopwords(full_corpus=texts, erase = True)
     docs_clean = [clean_text(doc, custom_stopwords).split() for doc in texts]
-    ldamodel = run_LDA(docs=docs_clean, num_topics=NUM_TOPICS, passes=50)
+    ldamodel = run_LDA(docs=docs_clean,
+                       num_topics=NUM_TOPICS,
+                       passes=20,
+                       alpha = 'auto',
+                       eta = 'auto')
     print(ldamodel.print_topics(num_topics=NUM_TOPICS, num_words=3))
 
 
