273d2d10501fac229b6eb98e9c77af55ea00c3f3
jmoro0408 <james.moro.11@gmail.com>
2023-01-02 23:06:03 +0000

    updated to use id2token

--- topic_modelling.py
+++ topic_modelling.py
@@ -10,41 +10,45 @@ from preprocessing import preprocess_main
 
 
 def run_LDA(
-    docs: list[str], index_dictionary: gensim.corpora.Dictionary, **kwargs
+    corpus: list[str], id2word: gensim.corpora.Dictionary, **kwargs
 ) -> tuple[gensim.models.ldamodel.LdaModel, list]:
     """Creates and runs the LDA model.
 
     Args:
-        docs (list[str]): List of text (strings) to be used in the analysis.
+        corpus (list[str]): List of text (strings) to be used in the analysis.
+        id2word (gensim.corpora.Dictionary): index to word dictionary.
         **kwargs: Other gensim LDA model arguments/hyperparameters
 
     Returns:
         gensim.models.ldamodel.LdaModel: Trained LDA model
     """
-    doc_term_matrix = [index_dictionary.doc2bow(doc) for doc in docs]
     Lda = gensim.models.ldamodel.LdaModel
-    return Lda(doc_term_matrix, id2word=index_dictionary, **kwargs)
+    return Lda(corpus, id2word=id2word, **kwargs)
 
 
 def main():
     """Main func to preprocess and subsequently run the LDA model."""
     # Preprocessing text
     NUM_ROWS = None  # no. rows (episodes) to grab from db
-    docs_clean, index_dictionary = preprocess_main(
+    corpus, index_dictionary = preprocess_main(
         num_rows_db=NUM_ROWS, save_preprocessed_text=True
     )
+    _ = index_dictionary[0]  # This is only to "load" the dictionary.
+    id2word = index_dictionary.id2token
     # Training Parameters
-    num_topics = 122 #From optimize_num_topics
-    passes = 3
+    num_topics = 122
+    passes = 20
     iterations = 400
+    eval_every = 10
 
     ldamodel = run_LDA(
-        docs=docs_clean,
-        index_dictionary=index_dictionary,
+        corpus=corpus,
+        id2word=id2word,
         num_topics=num_topics,
         passes=passes,
         iterations=iterations,
         chunksize = 2044,
+        eval_every = eval_every,
         alpha="auto",
         eta="auto",
     )
