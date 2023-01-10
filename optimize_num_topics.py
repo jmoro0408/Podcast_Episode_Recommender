4f41373ba2990f210f120af7064bb69407373ada
jmoro0408 <james.moro.11@gmail.com>
2023-01-10 10:34:29 +0000

    added preprocessing_main

--- optimize_num_topics.py
+++ optimize_num_topics.py
@@ -5,6 +5,8 @@ import gensim
 import matplotlib.pyplot as plt
 from gensim.models import CoherenceModel, LdaModel
 
+from preprocessing import preprocess_main
+
 logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s", level=logging.DEBUG
 )
@@ -31,41 +33,42 @@ def evaluate_graph(
     c_v : Coherence values corresponding to the LDA model with respective number of topics
     """
     c_v = []
-    u_mass = []
     lm_list = []
-    START = 2
+    START = 10
     STEP = 20
     _ = dictionary[0]  # This is only to "load" the dictionary.
     id2word = dictionary.id2token
 
     for num_topics in range(START, limit, STEP):
         print(f"-----------Testing {num_topics} topics.-----------")
+
+        passes = 15
+        iterations = 200
+        eval_every = 10
+        alpha = "auto"
+        eta = "symmetric"
+
         lm = LdaModel(
             corpus=corpus,
-            num_topics=num_topics,
             id2word=id2word,
-            passes=20,
-            iterations=400,
+            num_topics=num_topics,
+            passes=passes,
+            iterations=iterations,
             chunksize=2044,
-            alpha="auto",
-            eta="auto",
+            eval_every=eval_every,
+            alpha=alpha,
+            eta=eta,
         )
+
         lm_list.append(lm)
         cm = CoherenceModel(
             model=lm,
             texts=texts,
             dictionary=dictionary,
             coherence="c_v",
+            window_size = 100,
         )
         c_v.append(cm.get_coherence())
-
-        u_mass_model = CoherenceModel(
-            model=lm,
-            texts=texts,
-            dictionary=dictionary,
-            coherence="u_mass",
-        )
-        u_mass.append(u_mass_model.get_coherence())
 
     # Show graph
     x = range(START, limit, STEP)
@@ -76,29 +79,19 @@ def evaluate_graph(
     plt.legend(("C_v"), loc="best")
     plt.savefig("C_v_plot.png", format="png", facecolor="white")
     plt.show()
-    # u_mass plot
-    plt.plot(x, u_mass)
-    plt.xlabel("num_topics")
-    plt.ylabel("Coherence score")
-    plt.legend(("u_mass"), loc="best")
-    plt.savefig("u_mass_plot.png", format="png", facecolor="white")
-    plt.show()
     pickle.dump(dict(zip(lm_list, c_v)), open("c_v_optimization_results.pkl", "wb"))
-    pickle.dump(
-        dict(zip(lm_list, u_mass)), open("u_mass_optimization_results.pkl", "wb")
-    )
-
-    return lm_list, c_v, u_mass
+    return lm_list, c_v
 
 
 if __name__ == "__main__":
+    NUM_ROWS = None  # no. rows (episodes) to grab from db
+    corpus, index_dictionary = preprocess_main(
+        num_rows_db=NUM_ROWS, save_preprocessed_text=True
+    )
     with open("cleaned_docs.pkl", "rb") as f:
         docs = pickle.load(f)
-    with open("corpus.pkl", "rb") as f:
-        corpus = pickle.load(f)
-    with open("index_dict.pkl", "rb") as f:
-        index_dict = pickle.load(f)
+
     # This takes around 7 hrs to run with 500 topics
-    lm_list, c_v, u_mass = evaluate_graph(
-        dictionary=index_dict, corpus=corpus, texts=docs, limit=500
+    lm_list, c_v, = evaluate_graph(
+        dictionary=index_dictionary, corpus=corpus, texts=docs, limit=300
     )
